c38c991f5822b690056bf04d2427b41ecc7361f7
viraj <anchan.viraj@gmail.com>
2019-06-23 22:52:12 +0530

    disable animation

--- src/components/ListBookmarks.jsx
+++ src/components/ListBookmarks.jsx
@@ -25,7 +25,7 @@ class ListBookmarks extends Component {
         <p className="h3" style={{textTransform: 'capitalize'}}>{tag} Bookmarks</p>
         <br/>
         {isLoading && <span>Fetching Bookmarks...</span>}
-        <StackGrid columnWidth={width <= 768 ? '100%' : '33.33%'} gutterWidth={10} gutterHeight={10}>
+        <StackGrid columnWidth={width <= 768 ? '100%' : '33.33%'} gutterWidth={10} gutterHeight={10} duration={0}>
           {bookmarks.filter(bookmark => bookmark.tags.includes(tag) || tag === "all").map((bookmark) => (
 
             <Card key={bookmark.id + width}>
