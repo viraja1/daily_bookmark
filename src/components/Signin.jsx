b6c5de0a67e67fbfc50b3bc0b4ea832a74e7d565
viraj <anchan.viraj@gmail.com>
2019-12-25 21:33:14 +0530

    update login page

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Image} from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default class Signin extends Component {
   constructor(props) {
@@ -9,17 +10,30 @@ export default class Signin extends Component {
   render() {
     const {handleSignIn} = this.props;
     return (
-      <div className="panel-landing  h-100 d-flex" id="section-1">
-        <div className="jumbotron" style={{margin: "auto", textAlign: "center", padding: "5%"}}>
-          <div>
-            <p className="h1">Daily Bookmark</p>
+      <div>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="/"><img src="/icon.png" width="40" height="40"/> Daily Bookmark</Navbar.Brand>
+        </Navbar>
+        <div className="container" style={{marginTop: "50px"}}>
+          <div className="row">
+            <div className="col-md-5">
+              <p className="h3">Privacy focussed bookmark manager</p>
+              <br/>
+              <p>Bookmarks are stored by Daily Bookmark in an encrypted form.</p>
+              <p>Bookmarks can be accessed only by you since it is stored in your own private gaia storage in an
+                encrpyted form.</p>
+              <p>Daily Bookmark is a simple secure app for your daily bookmark needs.</p>
+              <br/>
+              <Button variant="secondary btn-lg" onClick={handleSignIn.bind(this)}>Login</Button>
+              <br/>
+              <br/>
+            </div>
+            <div className="col-md-7">
+              <Image src="/bookmark_home.png" responsive style={{width: "100%", height: 'auto'}}/>
+              <br/>
+              <br/>
+            </div>
           </div>
-          <div>
-            <br/>
-            <p>Simple secure app for your daily bookmark needs</p>
-          </div>
-          <br/>
-          <Button variant="secondary btn-lg" onClick={handleSignIn.bind(this)}>Login with Blockstack</Button>
         </div>
       </div>
     )
